89d066fa1f9fb96c366ef486f32eb663fde9090e
ekity1002 <dreamguidance1@gmail.com>
2024-01-21 15:18:09 +0900

    Add Todo validation test

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,12 @@
 class Todo < ApplicationRecord
+  validates :title, presence: true
+  validates :description, length: {maximum: 500}
+  validate :due_date_cannot_be_in_the_past
+
+  private
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:todo) do
+    Todo.new(
+      title: "Test Todo",
+      description: "Test description",
+      due_date: Date.tomorrow,
+      completed: true
+    )
+  end
+
+  it "is valid with a title, description, completed, due_date" do
+    expect(todo).to be_valid
+  end
+
+  context "when title is missing" do
+    it "is invalid" do
+      todo.title = nil
+      expect(todo).not_to be_valid
+    end
+  end
+
+  context "when description is over 500 chars" do
+    it "is invalid" do
+      todo.description = "a"*501
+      expect(todo).not_to be_valid
+    end
+  end
+
+  context "due date is not future" do
+    it "is invalid" do
+      todo.due_date = Date.yesterday
+      expect(todo).not_to be_valid
+    end
+  end
 end
